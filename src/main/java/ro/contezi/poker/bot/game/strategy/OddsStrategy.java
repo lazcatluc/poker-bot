690b9cb68d7c57fde4a99e8efa9cb7f37ef9fbe0
Catalin Lazar <lazcatluc@gmail.com>
2015-12-25 16:56:45 +0200

    simplify structure of odds strategy

--- src/main/java/ro/contezi/poker/bot/game/strategy/OddsStrategy.java
+++ src/main/java/ro/contezi/poker/bot/game/strategy/OddsStrategy.java
@@ -1,37 +1,23 @@
 package ro.contezi.poker.bot.game.strategy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import ro.contezi.poker.bot.cards.Card;
-import ro.contezi.poker.bot.cards.Rank;
-import ro.contezi.poker.bot.cards.Suit;
 import ro.contezi.poker.bot.game.simulator.ManyPlayerSimulator;
 
 public class OddsStrategy implements Strategy {
 
-	private final ManyPlayerSimulator.Builder manyPlayerSimulator;
-	private final List<Card> myCards;
-	private final List<Card> communityCards;
+	private final ManyPlayerSimulator manyPlayerSimulator;
 	private final int pot;
 	private final int amountRequiredToCheck;
-	private final int players;
-
+	
 	private OddsStrategy(Builder builder) {
-		this.manyPlayerSimulator = builder.manyPlayerSimulator;
-		this.myCards = new ArrayList<>(builder.myCards);
-		this.communityCards = new ArrayList<>(builder.communityCards);
+		this.manyPlayerSimulator = builder.manyPlayerSimulatorBuilder.build();
 		this.pot = builder.pot;
 		this.amountRequiredToCheck = builder.amountRequiredToCheck;
-		this.players = builder.players;
 	}
 
 	@Override
 	public Action getAction() {
-		double odds = manyPlayerSimulator.withCommunity(communityCards).withCurrent(myCards).withPlayers(players)
-				.build().getOdds();
+		double odds = manyPlayerSimulator.getOdds();
 		if (amountRequiredToCheck > odds * (pot + amountRequiredToCheck)) {
 			return Action.FOLD;
 		}
@@ -39,12 +25,9 @@ public class OddsStrategy implements Strategy {
 	}
 
 	public static class Builder {
-		private ManyPlayerSimulator.Builder manyPlayerSimulator = new ManyPlayerSimulator.Builder();
-		private List<Card> communityCards = Collections.emptyList();
-		private List<Card> myCards = Arrays.asList(Card.get(Suit.SPADE, Rank.ACE), Card.get(Suit.HEART, Rank.ACE));
+		private ManyPlayerSimulator.Builder manyPlayerSimulatorBuilder = new ManyPlayerSimulator.Builder();
 		private int pot = 3;
 		private int amountRequiredToCheck = 1;
-		private int players = 2;
 
 		public Builder withPot(int pot) {
 			this.pot = pot;
@@ -57,12 +40,12 @@ public class OddsStrategy implements Strategy {
 		}
 
 		public Builder withMyCards(Card... cards) {
-			myCards = Arrays.asList(cards);
+			manyPlayerSimulatorBuilder.withCurrent(cards);
 			return this;
 		}
 
 		public Builder withPlayers(int players) {
-			this.players = players;
+			manyPlayerSimulatorBuilder.withPlayers(players);
 			return this;
 		}
 
@@ -71,7 +54,7 @@ public class OddsStrategy implements Strategy {
 		}
 
 		public Builder withManyPlayerSimulatorBuilder(ManyPlayerSimulator.Builder manyPlayerSimulatorBuilder) {
-			this.manyPlayerSimulator = manyPlayerSimulatorBuilder;
+			this.manyPlayerSimulatorBuilder = manyPlayerSimulatorBuilder;
 			return this;
 		}
 	}
--- src/test/java/ro/contezi/poker/bot/game/strategy/OddsStrategyTest.java
+++ src/test/java/ro/contezi/poker/bot/game/strategy/OddsStrategyTest.java
@@ -1,12 +1,8 @@
 package ro.contezi.poker.bot.game.strategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,15 +14,11 @@ public class OddsStrategyTest {
 	private ManyPlayerSimulator manyPlayerSimulator;
 	private ManyPlayerSimulator.Builder manyPlayerSimulatorBuilder;
 	
-	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() {
 		manyPlayerSimulator = mock(ManyPlayerSimulator.class);
 		manyPlayerSimulatorBuilder = mock(ManyPlayerSimulator.Builder.class);
 		when(manyPlayerSimulatorBuilder.build()).thenReturn(manyPlayerSimulator);
-		when(manyPlayerSimulatorBuilder.withCommunity(any(List.class))).thenReturn(manyPlayerSimulatorBuilder);
-		when(manyPlayerSimulatorBuilder.withCurrent(any(List.class))).thenReturn(manyPlayerSimulatorBuilder);
-		when(manyPlayerSimulatorBuilder.withPlayers(anyInt())).thenReturn(manyPlayerSimulatorBuilder);
 	}
 	
 	@Test
